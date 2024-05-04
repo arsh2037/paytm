cdcc57ff2b312e8a15c446fdd79cf1d1660e6ec3
arsh2037 <arshhasan68@gmail.com>
2024-05-04 17:15:59 +0930

    added middlewares

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,19 @@ import express from "express";
 const mainRouter= require("./routes/index.js");
 const userRouter= require("./routes/user.js");
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+app.use(bodyParser.json());
+
 
 const app = express();
+
+app.use(cors());
 app.use("/api/v1", mainRouter);
 
 app.use("/api/v1/user",userRouter);
 
+app.listen(3000)
 // task in hand Create a router that routes all request s to /api/v1 
 
