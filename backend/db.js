4e1e141c3dc5ff51ba7c32200753264e5fdd66fb
arsh2037 <arshhasan68@gmail.com>
2024-05-05 14:28:44 +0930

    transaction schema added

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { type } = require('os');
 
 mongoose.connect("mongodb+srv://arshhasan68:VnE6pFknwX3dBhDo@cluster0.amu8y8b.mongodb.net/");
 
@@ -9,6 +10,20 @@ const userSchema = new mongoose.Schema({
     isAdmin: Boolean
 });
 
-const User = mongoose.model('User', userSchema);
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance : {
+        type : Number,
+        required: true
+    }
 
-module.exports = User;
+});
+
+const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = {User, Account};
